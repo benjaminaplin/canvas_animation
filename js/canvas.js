9d6bac3e0fba5ffbf1870a574ebdf9cb911b8c5b
Benjamin Aplin <benjaminaplin@gmail.com>
2015-12-17 13:35:48 -0500

    add comments

--- js/canvas.js
+++ js/canvas.js
@@ -1,19 +1,26 @@
 console.log('canvas linked!')
 
+//create images with image constructor
 var image1 = new Image(),
     image2 = new Image(),
     image3 = new Image();
 
+
+//set X and Y positions of each image
 var img1PosX = 50,
     img1PosY = 50,
     img3PosX = 150,
     img3PosY = 150,
     img2PosX = 100,
     img2PosY = 100,
+    //set width and height of image2, which expands at the end of animation    
     img2width = 100,
     img2height = 100,
-    forward = true,
+    //set a boolean to control the order of animation    
+    runAnimationInOrder = true,
+    //set speed of movement of squares    
     speed = 2,
+    //set speed of expansion of image2    
     img2speed = 2;
 
  var requestAnimationFrame = window.requestAnimationFrame || 
@@ -21,7 +28,8 @@ var img1PosX = 50,
                             window.webkitRequestAnimationFrame || 
                             window.msRequestAnimationFrame;
 
-function animation1(){
+// this function controls the all of the animation of the squares
+function animateSquares(){
     if (img1PosY <= 50 && img1PosY > 0){
       img1PosX -= speed;
       img1PosY -= speed;      
@@ -41,39 +49,43 @@ function animation1(){
       img3PosX -= speed;
     }
 
-
     if (speed == 0 && img2height <= 300) {
-      forward = false
-      console.log(forward)
+      runAnimationInOrder = false
       img2PosY -= img2speed;
       img2PosX -= img2speed;
       img2height += img2speed * 2;
       img2width += img2speed * 2;
     }
 }
-
+/* 
+  the 'animate' function requests the animation frame, 
+  defines the image sources, calls the animation frame function, 
+  and then fires the draw function, triggering each iteration of
+  drawing a new frame
+*/
 function animate(){
   requestAnimationFrame(animate);
   image1.src = 'http://riley.dev.kargo.com/code-test/test0.png'
   image2.src = 'http://riley.dev.kargo.com/code-test/test1.png'
   image3.src = 'http://riley.dev.kargo.com/code-test/test2.png'
-  animation1();
+  animateSquares();
   requestAnimationFrame(draw);
 }
 
+// the draw function does the drawing
 function draw(){
+  //declares variables for draw function for image2
   var image, dx, dy, dWidth, dHeight
+  //next lines select canvas element in HTML and 
+  //initializes size and background color of canvas
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext("2d");
-  ctx.globalCompositeOperation = "destination-over";
   ctx.canvas.width  = 300;
   ctx.canvas.height = 300;
   ctx.fillStyle = "#d3d3d3";
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  //somehow put these into an array and 
-  //draw them in a different order when the first two 
-  //animation sequences are finished
 
+  //the following three functions draw the animation of the squares
   var draw1 = function draw1(){
     ctx.drawImage(image1,img1PosX,img1PosY);
   }
@@ -85,35 +97,27 @@ function draw(){
   var draw3 = function draw3(){
     ctx.drawImage(image3,img3PosX,img3PosY);
   }
-
+  //this array sets the order squares are drawn in,
+  // making up for the lack of 'z-index' in canvas
   var drawArray = [draw1,draw2,draw3];
   
-  if (forward == true){
-    console.log('true', drawArray)
-    //if true, it runs 1,2,3
+  if (runAnimationInOrder == true){
+  //if true, it draws squares in order 1,2,3
    drawArray.forEach(function(e){
     e();
    });
   }
-  if(forward == false){
-    console.log('false', drawArray)
-
-    //if false, it runs 1,3,2
+  if(runAnimationInOrder == false){
+  //if false, it draws squares in order 1,3,2, so
+  //that image2 overlaps the others as it expands
     drawArray = [draw1,draw3,draw2];
     drawArray.forEach(function(e){
       e();
     });
+    // resets the order of the array
     drawArray = [draw1,draw2,draw3];
   }
-  
-
-  ctx.save();
-  var requestID = requestAnimationFrame(draw);
 } 
 
 animate();
-
-setTimeout(function(){ 
-  cancelAnimationFrame(requestID);
- }, 5000);
 
